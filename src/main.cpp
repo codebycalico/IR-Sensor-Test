5741ac29abc94c6c22779781f9ed3bcaa1cfe583
Calico Randall <calicorrandall@gmail.com>
2025-02-25 09:14:46 -0800

    Beginning to add lights
    incorporating the LED strip to be triggered by the IR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,30 @@
 #include <Arduino.h>
-#include <Bounce2.h>
+#include <FastLED.h>
 
-#define GREEN_BUTTON_PIN 1
-#define RED_BUTTON_PIN 0
-#define KEY KEY_UP
+#define LED_PIN 2
+#define TOTAL_LEDS_OUTER_RING 12
+#define TOTAL_LEDS_INNER_RING 18
 
-bool prevPressed = false;
-
-Bounce2::Button greenButton = Bounce2::Button();
-Bounce2::Button redButton = Bounce2::Button();
+CRGBArray <TOTAL_LEDS_OUTER_RING + TOTAL_LEDS_INNER_RING> ledStrip;
+uint16_t data;
 
 void setup() {
-  // Button setups
-  greenButton.attach(GREEN_BUTTON_PIN, INPUT_PULLUP);
-  redButton.attach(RED_BUTTON_PIN, INPUT_PULLUP);
-  greenButton.interval(5);
-  redButton.interval(5);
-  greenButton.setPressedState(LOW);
-  redButton.setPressedState(LOW);
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(ledStrip, TOTAL_LEDS_INNER_RING + TOTAL_LEDS_OUTER_RING);
 
-  // Serial and keyboard starts
   Serial.begin(9600);
-  Keyboard.begin();
+  Serial.println("Serial setup complete.");
 }
 
 void loop() {
-  greenButton.update();
-  redButton.update();
+  // Read data from the IR sensor
+  data = analogRead(A5);
+  Serial.println(data);
+  delay(100);
 
-  //This code alternates between presses.
-  //If it is pressed once that turns it on, if it is pressed again,
-  //that turns it off. This is taken care of in the Unity script
-  //but I added the functionality here too if we need it in the future.
-  // if(greenButton.pressed()){
-  //   if(prevPressed == false) {
-  //     prevPressed = true;
-  //     Keyboard.press(KEY);
-  //     Serial.println("UP key pressed.");
-  //     delay(20);
-  //   } else {
-  //     prevPressed = false;
-  //     Serial.println("UP key released.");
-  //     Keyboard.releaseAll();
-  //     delay(20);
-  //   }
-  // }
-
-  // if(greenButton.pressed()){
-  //   Keyboard.press(KEY);
-  //   Keyboard.releaseAll();
-  // }
-
-// If green button is pressed, then press the mouse
-// In the Unity program, this coordinates to creating water
-// in the simulation (adds more water when the "mouse" is pressed)
-  if(greenButton.pressed() && !prevPressed){
-    prevPressed = true;
-    Mouse.press();
-  } else if(greenButton.pressed() && prevPressed){
-    prevPressed = false;
-    Mouse.release();
-  }
-
-// If red button is pressed, press the key "up"
-// In the Unity program, this coordinates to clearing the water
-// from the simulation
-  if(redButton.pressed()){
-    Keyboard.press(KEY);
-    delay(20);
-    Keyboard.releaseAll();
+  // Trigger the lights on the ring
+  // if the data value within range
+  if(data < 100) {
+    fill_solid(ledStrip, TOTAL_LEDS_OUTER_RING, CRGB::Green);
+    FastLED.show();
   }
 }
