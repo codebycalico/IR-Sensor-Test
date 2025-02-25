ea20c770210595b00688ab359ead3b2fb286fb0f
Calico Randall <calicorrandall@gmail.com>
2025-02-25 10:13:48 -0800

    Completing the LEDs
    Completing the code to trigger the LEDs based on the distance from the IR sensor. LED ring is doing a rainbow wave until the IR sensor reads lower than 100, then the LED ring flashes green for a second.
    May switch to changing the brightness based on the proximity.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,30 @@
 
 #define LED_PIN 2
 #define TOTAL_LEDS_OUTER_RING 12
-#define TOTAL_LEDS_INNER_RING 18
+#define TOTAL_LEDS_INNER_RING 19
+#define TOTAL_LEDS (TOTAL_LEDS_INNER_RING + TOTAL_LEDS_OUTER_RING)
 
-CRGBArray <TOTAL_LEDS_OUTER_RING + TOTAL_LEDS_INNER_RING> ledStrip;
+CRGBArray <TOTAL_LEDS_OUTER_RING + TOTAL_LEDS_INNER_RING> ledRing;
+CRGB* ledInnerRing = &ledRing[TOTAL_LEDS_OUTER_RING];
+
 uint16_t data;
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, LED_PIN>(ledStrip, TOTAL_LEDS_INNER_RING + TOTAL_LEDS_OUTER_RING);
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(ledRing, TOTAL_LEDS_INNER_RING + TOTAL_LEDS_OUTER_RING);
 
   Serial.begin(9600);
   Serial.println("Serial setup complete.");
+}
+
+// The fill_rainbow call doesn't support brightness levels.
+void rainbow_wave(uint8_t thisSpeed, uint8_t deltaHue) {
+
+  // Simple rainbow wave
+  // uint8_t thisHue = beatsin8(thisSpeed,0,255);
+  // Simple rainbow march
+   uint8_t thisHue = beat8(thisSpeed,255);
+    
+   fill_rainbow(ledRing, TOTAL_LEDS, thisHue, deltaHue);
 }
 
 void loop() {
@@ -24,7 +38,32 @@ void loop() {
   // Trigger the lights on the ring
   // if the data value within range
   if(data < 100) {
-    fill_solid(ledStrip, TOTAL_LEDS_OUTER_RING, CRGB::Green);
+    fill_solid(ledRing, TOTAL_LEDS, CRGB::Black);
+    for(int i = 0; i < TOTAL_LEDS_OUTER_RING; i++){
+      ledRing[i] = CRGB::Green;
+      FastLED.show();
+      delay(50);
+    }
+    fill_solid(ledRing, TOTAL_LEDS, CRGB::Green);
+    FastLED.show();
+    delay(100);
+    fill_solid(ledRing, TOTAL_LEDS, CRGB::Black);
+    FastLED.show();
+    delay(100);
+    fill_solid(ledRing, TOTAL_LEDS, CRGB::Green);
+    FastLED.show();
+    delay(500);
+
+    // for(int i = 0; i < TOTAL_LEDS; i++) {
+    //   ledRing[i] = CRGB::Black;
+    //   FastLED.show();
+    //   delay(50);
+    // }
+    // fill_solid(ledRing, TOTAL_LEDS_OUTER_RING, CRGB::Green);
+    // fill_solid(ledInnerRing, TOTAL_LEDS_INNER_RING, CRGB::Black);
+    // FastLED.show();
+  } else {
+    rainbow_wave(10, 10);
     FastLED.show();
   }
 }
